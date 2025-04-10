425e1ad8d5b19d12f669bcf153a1ab0a65e970bd
Jitesh Yadav <jiteshyadav1990@gmail.com>
2025-04-09 21:07:29 -0400

    chore: gh workflow updated

--- packages/backend/.platform/hooks/prebuild/01_install_pnpm.sh
+++ packages/backend/.platform/hooks/prebuild/01_install_pnpm.sh
@@ -8,11 +8,14 @@ echo "Removing existing pnpm..."
 npm rm -g pnpm || true
 
 echo "Installing pnpm..."
-# Try installing pnpm with sudo
-sudo npm install -g pnpm@latest --no-fund --no-audit --force
+npm install -g pnpm@latest --no-fund --no-audit --force
 
 echo "Verifying pnpm installation..."
-# Verify installation
 pnpm --version
+
+echo "Installing dependencies..."
+# Install dependencies without frozen lockfile and ignore workspace protocol
+cd /var/app/staging
+pnpm install --no-frozen-lockfile --ignore-workspace
 
 echo "pnpm installation complete"
